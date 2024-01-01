a8d19753f5180b6cd7c15c1853941b9d4ace613b
PHP <polok1697@gmail.com>
2024-01-01 17:44:04 +0600

    heading

--- formvalidation.php
+++ formvalidation.php
@@ -50,7 +50,7 @@ if (isset($_POST['sub123']) && $_SERVER ['REQUEST_METHOD'] == "POST") {
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
+    <title>Form Valiadition</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
   </head>
   <body>
